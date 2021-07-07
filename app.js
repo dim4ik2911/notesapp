e2deb40ed010bb46745807c099fb092bd0791547
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-07-07 19:33:42 +0100

    added 4 commands using yargs package

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
-const validator = require("validator");
+const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
-const life = getNotes();
-console.log(life);
 
-console.log(validator.isEmail("@gmail.com"));
+// Customize yargs version
+yargs.version("1.1.0");
 
-console.log(validator.isURL("google.com"));
+//add , remove, read, list
+
+// create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: () => {
+    console.log("Adding a new note");
+  },
+});
+
+// create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: () => {
+    console.log("Remove a new note");
+  },
+});
+
+//create list command
+yargs.command({
+  command: "list",
+  describe: "list a note",
+  handler: () => {
+    console.log("list a note");
+  },
+});
+//create read command
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  handler: () => {
+    console.log("read a note");
+  },
+});
+
+console.log(yargs.argv);
